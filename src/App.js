a384df68a347af63b8e026d5d18d585e74540b84
Joss Doebler <mail@joss-doebler.de>
2020-07-29 15:33:06 +0200

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+import ChooseMeme from './pages/choose-meme/ChooseMeme';
+import Editor from './pages/editor/Editor';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/">
+          <ChooseMeme />
+        </Route>
+        <Route path="/editor">
+          <Editor />
+        </Route>
+      </Switch>
     </div>
   );
 }

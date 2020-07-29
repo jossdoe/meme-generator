9cb0b55ae67cf0c0763b7f77183a13fcccf3f514
Joss Doebler <mail@joss-doebler.de>
2020-07-29 17:41:43 +0200

    Styling for input fields

--- src/components/input-fields/InputFields.js
+++ src/components/input-fields/InputFields.js
@@ -4,10 +4,13 @@ import css from './css.module.css';
 const InputFields = () => {
   const [topValue, setTopValue] = useState('');
   const [bottomValue, setBottomValue] = useState('');
+  const [fontFamily, setFontFamily] = useState('Impact');
+  const [fontSize, setFontSize] = useState(16);
 
   const onChangeTop = (e) => setTopValue(e.currentTarget.value);
-
   const onChangeBottom = (e) => setBottomValue(e.currentTarget.value);
+  const onChangeFontFamily = (e) => setFontFamily(e.target.value);
+  const onChangeFontSize = (e) => setFontSize(e.target.value);
 
   return (
     <div className={css.inputFields}>
@@ -28,6 +31,30 @@ const InputFields = () => {
           value={bottomValue}
           onChange={onChangeBottom}
         />
+      </div>
+      <div className={css.dropdownContainer}>
+        <div>
+          <label htmlFor="font-family">Font Family</label>
+          <select
+            id="font-family"
+            value={fontFamily}
+            onChange={onChangeFontFamily}
+          >
+            <option value="Impact">Impact</option>
+            <option value="Arial">Arial</option>
+            <option value="Arial Black">Arial Black</option>
+            <option value="Comic Sans">Comic Sans</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="font-size">Font Size</label>
+          <select id="font-size" value={fontSize} onChange={onChangeFontFamily}>
+            <option value="16">16</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+            <option value="42">42</option>
+          </select>
+        </div>
       </div>
     </div>
   );

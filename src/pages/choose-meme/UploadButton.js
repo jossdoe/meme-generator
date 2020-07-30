dd7529900f930974cf61e704c0af09ed8d8d13bd
Jakub Jureczko <untech.untech@gmail.com>
2020-07-30 12:29:53 +0200

    das

--- src/pages/choose-meme/UploadButton.js
+++ src/pages/choose-meme/UploadButton.js
@@ -5,7 +5,9 @@ import logo from '../../assets/logo/logo1.png'
 function UploadButton() {
   return(
     <div className="uploadButton">
-      <img src={logo} alt='logo' ></img>
+      <div className="logo">
+      <img src={logo} alt='logo' ></img>     
+      </div>
       <button>UPLOAD YOUR PICTURE</button>
     </div>
   )

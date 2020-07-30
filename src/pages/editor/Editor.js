f9909d6b5e868a57bbd1a2a98e3970b5d76d49ca
Joss Doebler <mail@joss-doebler.de>
2020-07-30 11:35:12 +0200

    Styling for meme area and text placement on image

--- src/pages/editor/Editor.js
+++ src/pages/editor/Editor.js
@@ -8,8 +8,12 @@ const Editor = () => {
       <nav className={css.navigation}>
         <EditorNav />
       </nav>
-      <section className={css.meme}>
-        <img src="https://via.placeholder.com/500" alt="" />
+      <section className={css.memeContainer}>
+        <div className={css.meme}>
+          <img src="https://via.placeholder.com/500" alt="" />
+          <div className={css.topText}>This is top text</div>
+          <div className={css.bottomText}>This is bottom text</div>
+        </div>
       </section>
     </main>
   );

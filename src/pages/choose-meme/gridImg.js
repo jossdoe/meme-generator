9acc7f39ed1d794c6b88f616dc2c4c18fec2df95
Joss Doebler <mail@joss-doebler.de>
2020-07-31 13:31:50 +0200

    Connected template data to editor logic

--- src/pages/choose-meme/gridImg.js
+++ src/pages/choose-meme/gridImg.js
@@ -1,52 +1,148 @@
-import img1 from "./img/1.jpg";
-import img2 from "./img/2.jpg";
-import img3 from "./img/3.jpg";
-import img4 from "./img/4.jpg";
-import img5 from "./img/5.jpg";
-import img6 from "./img/6.jpg";
-import img7 from "./img/7.jpg";
-import img8 from "./img/8.jpg";
+import img1 from './img/1.jpg';
+import img2 from './img/2.jpg';
+import img3 from './img/3.jpg';
+import img4 from './img/4.jpg';
+import img5 from './img/5.jpg';
+import img6 from './img/6.jpg';
+import img7 from './img/7.jpg';
+import img8 from './img/8.jpg';
 
 const gridImg = [
   {
     id: 1,
     src: img1,
+    cssTop: {
+      top: '0px',
+      bottom: null,
+      left: '0px',
+      right: '0px'
+    },
+    cssBottom: {
+      top: null,
+      bottom: '0px',
+      left: '0px',
+      right: '0px'
+    }
   },
 
   {
     id: 2,
     src: img5,
+    cssTop: {
+      top: '0px',
+      bottom: null,
+      left: '0px',
+      right: '0px'
+    },
+    cssBottom: {
+      top: null,
+      bottom: '0px',
+      left: '0px',
+      right: '0px'
+    }
   },
 
   {
     id: 3,
     src: img3,
+    cssTop: {
+      top: '0px',
+      bottom: null,
+      left: '0px',
+      right: '0px'
+    },
+    cssBottom: {
+      top: null,
+      bottom: '0px',
+      left: '0px',
+      right: '0px'
+    }
   },
 
   {
     id: 4,
     src: img4,
+    cssTop: {
+      top: '0px',
+      bottom: null,
+      left: '0px',
+      right: '0px'
+    },
+    cssBottom: {
+      top: null,
+      bottom: '0px',
+      left: '0px',
+      right: '0px'
+    }
   },
 
   {
     id: 5,
     src: img2,
+    cssTop: {
+      top: '0px',
+      bottom: null,
+      left: '0px',
+      right: '0px'
+    },
+    cssBottom: {
+      top: null,
+      bottom: '0px',
+      left: '0px',
+      right: '0px'
+    }
   },
 
   {
     id: 6,
     src: img6,
+    cssTop: {
+      top: '0px',
+      bottom: null,
+      left: '0px',
+      right: '0px'
+    },
+    cssBottom: {
+      top: null,
+      bottom: '0px',
+      left: '0px',
+      right: '0px'
+    }
   },
 
   {
     id: 7,
     src: img7,
+    cssTop: {
+      top: '0px',
+      bottom: null,
+      left: '0px',
+      right: '0px'
+    },
+    cssBottom: {
+      top: null,
+      bottom: '0px',
+      left: '0px',
+      right: '0px'
+    }
   },
 
   {
     id: 8,
     src: img8,
-  },
+    cssTop: {
+      top: '0px',
+      bottom: null,
+      left: '0px',
+      right: '0px'
+    },
+    cssBottom: {
+      top: null,
+      bottom: '0px',
+      left: '0px',
+      right: '0px'
+    }
+  }
 ];
 
 export default gridImg;
--- src/pages/editor/Editor.js
+++ src/pages/editor/Editor.js
@@ -2,61 +2,11 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import css from './css.module.css';
 import EditorNav from '../../components/editor-nav/EditorNav';
-
-const testData = [
-  {
-    id: 1,
-    src: 'https://via.placeholder.com/500',
-    cssTop: {
-      top: '0px',
-      bottom: null,
-      left: '0px',
-      right: '0px'
-    },
-    cssBottom: {
-      top: null,
-      bottom: '0px',
-      left: '0px',
-      right: '0px'
-    }
-  },
-  {
-    id: 2,
-    src: 'https://via.placeholder.com/400',
-    cssTop: {
-      top: '0px',
-      bottom: null,
-      left: '0px',
-      right: '0px'
-    },
-    cssBottom: {
-      top: null,
-      bottom: '0px',
-      left: '0px',
-      right: '0px'
-    }
-  },
-  {
-    id: 3,
-    src: 'https://via.placeholder.com/450',
-    cssTop: {
-      top: '0px',
-      bottom: null,
-      left: '0px',
-      right: '0px'
-    },
-    cssBottom: {
-      top: null,
-      bottom: '0px',
-      left: '0px',
-      right: '0px'
-    }
-  }
-];
+import gridImg from '../choose-meme/gridImg';
 
 const Editor = () => {
   const { id } = useParams();
-  const image = testData.find((item) => item.id === parseInt(id));
+  const image = gridImg.find((item) => item.id === parseInt(id));
 
   const [topValue, setTopValue] = useState('Top Text');
   const [bottomValue, setBottomValue] = useState('Bottom Text');

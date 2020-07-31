6b544a29cf0e8bbee376553498a7a9b53240fc5b
Joss Doebler <mail@joss-doebler.de>
2020-07-31 13:34:26 +0200

    Code formatting

--- src/components/input-fields/InputFields.js
+++ src/components/input-fields/InputFields.js
@@ -11,45 +11,48 @@ const InputFields = ({ editorState }) => {
   return (
     <div className={css.inputFields}>
       <div>
-        <label htmlFor="text-top">Text: Top</label>
+        <label htmlFor='text-top'>Text: Top</label>
         <input
-          id="text-top"
-          type="text"
+          id='text-top'
+          type='text'
           value={editorState.topValue}
           onChange={onChangeTop}
         />
       </div>
       <div>
-        <label htmlFor="text-bottom">Text: Bottom</label>
+        <label htmlFor='text-bottom'>Text: Bottom</label>
         <input
-          id="text-bottom"
-          type="text"
+          id='text-bottom'
+          type='text'
           value={editorState.bottomValue}
           onChange={onChangeBottom}
         />
       </div>
       <div className={css.dropdownContainer}>
         <div>
-          <label htmlFor="font-family">Font Family</label>
+          <label htmlFor='font-family'>Font Family</label>
           <select
-            id="font-family"
+            id='font-family'
             value={editorState.fontFamily}
             onChange={onChangeFontFamily}
           >
-            <option value="Impact">Impact</option>
-            <option value="Arial Black">Arial Black</option>
+            <option value='Impact'>Impact</option>
+            <option value='Arial Black'>Arial Black</option>
           </select>
         </div>
         <div>
-          <label htmlFor="font-size">Font Size</label>
+          <label htmlFor='font-size'>Font Size</label>
           <select
-            id="font-size"
+            id='font-size'
             value={editorState.fontSize}
             onChange={onChangeFontSize}
           >
-            <option value="30">30</option>
-            <option value="42">42</option>
-            <option value="54">54</option>
+            <option value='18'>18</option>
+            <option value='22'>22</option>
+            <option value='26'>26</option>
+            <option value='30'>30</option>
+            <option value='42'>42</option>
+            <option value='54'>54</option>
           </select>
         </div>
       </div>

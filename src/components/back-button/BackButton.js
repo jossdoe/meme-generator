480b20fe2a86a14054eb4503dd99a8e9f57acdd7
Usamoto <46085749+Usamoto@users.noreply.github.com>
2020-07-31 11:48:58 +0200

    add back-arrow

--- src/components/back-button/BackButton.js
+++ src/components/back-button/BackButton.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const BackButton = () => <div>BackButton</div>;
+import { Icon } from "@iconify/react";
+import roundArrowBackIos from "@iconify/icons-ic/round-arrow-back-ios";
+
+import css from "./css.module.css";
+import { Link } from "react-router-dom";
+
+const BackButton = () => (
+  <Link className={css.link} to="/">
+    <div className={css.top}>
+      <Icon icon={roundArrowBackIos} color="white" />
+      <p>Back to Galery</p>
+    </div>
+  </Link>
+);
 
 export default BackButton;

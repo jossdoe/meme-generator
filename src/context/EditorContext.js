85b77106a93a945a6c96093b82be732c5885cdef
Joss Doebler <mail@joss-doebler.de>
2020-08-03 17:32:15 +0200

    Replaced prop chain with EditorContext

--- src/context/EditorContext.js
+++ src/context/EditorContext.js
@@ -1,9 +1,51 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
+
+import { default as templates } from '../pages/choose-meme/gridImg';
+
+const initialState = {
+  template: templates[0],
+  textFields: [],
+  fontFamily: 'Impact',
+  fontSize: 18
+};
+
+const editorReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_TEMPLATE':
+      const newTemplate =
+        templates.find((item) => item.id === action.id) || templates[0];
+      return {
+        ...state,
+        template: newTemplate,
+        textFields: newTemplate.text.map(() => 'Your Text')
+      };
+
+    case 'SET_TEXTFIELD':
+      const newFields = [...state.textFields];
+      newFields[action.id] = action.value;
+      return { ...state, textFields: [...newFields] };
+
+    case 'SET_FONTFAMILY':
+      return { ...state, fontFamily: action.value };
+
+    case 'SET_FONTSIZE':
+      return { ...state, fontSize: action.value };
+
+    default:
+      return { ...state };
+  }
+};
 
 export const EditorContext = createContext({});
 
 const EditorContextProvider = ({ children }) => {
-  return <EditorContext.Provider>{children}</EditorContext.Provider>;
+  const [state, dispatch] = useReducer(editorReducer, initialState);
+
+  return (
+    <EditorContext.Provider value={{ state, dispatch }}>
+      {children}
+    </EditorContext.Provider>
+  );
 };
 
 export default EditorContextProvider;

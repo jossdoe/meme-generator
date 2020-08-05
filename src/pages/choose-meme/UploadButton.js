edb71967c10bb662003b5cf5b046b5332cba7736
Jakub Jureczko <untech.untech@gmail.com>
2020-08-05 14:59:02 +0200

    focus on input and media queries 4K

--- src/pages/choose-meme/UploadButton.js
+++ src/pages/choose-meme/UploadButton.js
@@ -3,39 +3,29 @@ import './UploadButton.css';
 import logo from '../../assets/logo/logo1.png';
 import { useHistory } from 'react-router-dom';
 
-const myStyle = {
-  backgroundColor: 'rgb(44, 43, 43)',
-  border: '0.1em solid rgb(247, 200, 115)',
-  color: 'rgb(247, 200, 115)',
-  borderRadius: '0.2em',
-  transition: '0.5s'
-};
 
 function UploadButton() {
   let history = useHistory();
   const [inputValue, setInputValue] = useState('');
 
-  const [style, setStyle] = useState(myStyle);
+  
 
   function handleClick() {
     history.push(`/editor/custom?url=${inputValue}`);
   }
-  function changeStyle(e) {
-    
-  }
-
+   
   return (
     <div className='uploadButton'>
       <div className='logo'>
         <img src={logo} alt='logo' />
       </div>
 
-      <input
+      <input 
         type='text'
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
-      <button onClick={handleClick}>UPLOAD YOUR PICTURE</button>
+      <button onClick={handleClick}>UPLOAD YOUR URL</button>
     </div>
   );
 }

d91136bc8ec4cc46c2ac718fa3cd5adcd76fbb5d
Jakub Jureczko <untech.untech@gmail.com>
2020-08-05 16:18:36 +0200

    media queries for input

--- src/pages/choose-meme/UploadButton.js
+++ src/pages/choose-meme/UploadButton.js
@@ -2,32 +2,31 @@ import React, { useState } from 'react';
 import './UploadButton.css';
 import logo from '../../assets/logo/logo1.png';
 import { useHistory } from 'react-router-dom';
-import { FaUpload } from "react-icons/fa";
-
+import { FaUpload } from 'react-icons/fa';
 
 function UploadButton() {
   let history = useHistory();
   const [inputValue, setInputValue] = useState('');
 
-  
-
   function handleClick() {
     history.push(`/editor/custom?url=${inputValue}`);
   }
-   
+
   return (
     <div className='uploadButton'>
       <div className='logo'>
         <img src={logo} alt='logo' />
       </div>
-
-      <input 
-        type='text'
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <button onClick={handleClick}><FaUpload /></button>
-      
+      <div className='inputField'>
+        <input
+          type='text'
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={handleClick}>
+          <FaUpload />
+        </button>
+      </div>
     </div>
   );
 }

87f947351a0ac8b88222be1d3d0fb00f8676dc3a
Jakub Jureczko <untech.untech@gmail.com>
2020-08-10 17:39:53 +0200

    validation URL alert button

--- src/pages/choose-meme/UploadButton.js
+++ src/pages/choose-meme/UploadButton.js
@@ -7,26 +7,28 @@ import { FaUpload } from 'react-icons/fa';
 function UploadButton() {
   let history = useHistory();
   const [inputValue, setInputValue] = useState('');
-  
-  
+
   function handleClick() {
-    if(inputValue === '') {
+    if (inputValue === '') {
       alert('Paste your Url');
-    }else {
+    } else if (!inputValue.includes('http://')) {
+      alert('This is not URL!!!');
+    } else {
       history.push(`/editor/custom?url=${inputValue}`);
     }
   }
 
   function handleEnter(e) {
-    if(e.keyCode === 13){
-      if(inputValue === '') {
+    if (e.keyCode === 13) {
+      if (inputValue === '') {
         alert('Paste your Url');
-      }else {
+      } else if (!inputValue.includes('http://')) {
+        alert('This is not URL!!!');
+      } else {
         history.push(`/editor/custom?url=${inputValue}`);
       }
     }
   }
-
 
   return (
     <div className='uploadButton'>
@@ -39,7 +41,7 @@ function UploadButton() {
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           onKeyDown={handleEnter}
-          autoComplete="off"
+          autoComplete='off'
         />
         <button onClick={handleClick}>
           <FaUpload />
